5e887bd8ab11ebf8fb9ec2a6f9a352f4963ad19e
Muhammed Kara <n.muhammed.kara@gmail.com>
2025-01-15 22:50:36 +0100

    containerize as much as possible

--- metadata/cmd/main.go
+++ metadata/cmd/main.go
@@ -2,41 +2,40 @@ package main
 
 import (
 	"context"
-	"flag"
 	"fmt"
 	"log"
 	"net"
 	"net/http"
 	"time"
 
+	"github.com/karaMuha/go-movie/metadata/config"
 	"github.com/karaMuha/go-movie/metadata/internal/core"
 	"github.com/karaMuha/go-movie/metadata/internal/core/ports/driving"
 	grpchandler "github.com/karaMuha/go-movie/metadata/internal/endpoint/grpc"
 	"github.com/karaMuha/go-movie/metadata/internal/endpoint/rest/v1"
 	"github.com/karaMuha/go-movie/metadata/internal/repository/memory"
 	"github.com/karaMuha/go-movie/pb"
+	"github.com/karaMuha/go-movie/pkg/database/postgres"
 	"github.com/karaMuha/go-movie/pkg/discovery"
 	consul "github.com/karaMuha/go-movie/pkg/discovery/consul"
 	"google.golang.org/grpc"
+
+	_ "github.com/lib/pq"
 )
 
 const serviceName = "metadata"
-const domain = "localhost"
 
 func main() {
 	log.Println("Starting movie metadata service")
-	var port int
-	flag.IntVar(&port, "port", 8080, "API handler port")
-	flag.Parse()
-	log.Printf("Starting the metadata service on port %d", port)
+	config := config.NewConfig()
 
-	registry, err := consul.NewConsulRegistry("localhost:8500")
+	registry, err := consul.NewConsulRegistry(config.ConsulAddress)
 	if err != nil {
 		panic(err)
 	}
 	ctx := context.Background()
 	instanceID := discovery.GenerateInstanceID(serviceName)
-	if err := registry.Register(ctx, instanceID, serviceName, fmt.Sprintf("localhost:%d", port)); err != nil {
+	if err := registry.Register(ctx, instanceID, serviceName, fmt.Sprintf("localhost%s", config.Port)); err != nil {
 		panic(err)
 	}
 
@@ -51,11 +50,18 @@ func main() {
 
 	defer registry.Deregister(ctx, instanceID, serviceName)
 
+	connectionString := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", config.DbHost, config.DbPort, config.DbUser, config.DbPassword, config.DbName, config.DbSslMode)
+	fmt.Printf("DB Connection: %s\n", connectionString)
+	_, err = postgres.ConnectToDb(config.DbDriver, connectionString)
+	if err != nil {
+		panic(err)
+	}
+
 	metadataRepo := memory.New()
 	app := core.New(metadataRepo)
 
 	//startRest(&app, port)
-	startGrpc(&app, port)
+	startGrpc(&app, config.Domain, config.Port)
 }
 
 func setupRestEndpoints(mux *http.ServeMux, metadataHandlerV1 rest.MetadataHandlerV1) {
@@ -65,14 +71,14 @@ func setupRestEndpoints(mux *http.ServeMux, metadataHandlerV1 rest.MetadataHandl
 	mux.Handle("/v1/", http.StripPrefix("/v1", metadataV1))
 }
 
-func startRest(app driving.IApplication, port int) {
+func startRest(app driving.IApplication, port string) {
 	metadataHandlerV1 := rest.NewMetadataHandlerV1(app)
 
 	mux := http.NewServeMux()
 	setupRestEndpoints(mux, metadataHandlerV1)
 
 	server := &http.Server{
-		Addr:    fmt.Sprintf(":%d", port),
+		Addr:    port,
 		Handler: mux,
 	}
 
@@ -82,9 +88,9 @@ func startRest(app driving.IApplication, port int) {
 	}
 }
 
-func startGrpc(app driving.IApplication, port int) {
+func startGrpc(app driving.IApplication, domain string, port string) {
 	metdataDataHandlerGrpc := grpchandler.NewMetadataHandler(app)
-	address := fmt.Sprintf("%s:%d", domain, port)
+	address := fmt.Sprintf("%s%s", domain, port)
 
 	listener, err := net.Listen("tcp", address)
 	if err != nil {
