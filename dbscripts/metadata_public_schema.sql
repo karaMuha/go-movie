6610818b0945119d64dc9fc7606b2440151957e5
Muhammed Kara <n.muhammed.kara@gmail.com>
2025-02-22 00:15:50 +0100

    implement cronjob for metadata service

--- dbscripts/metadata_public_schema.sql
+++ dbscripts/metadata_public_schema.sql
@@ -6,3 +6,9 @@ CREATE TABLE IF NOT EXISTS metadata(
   description TEXT,
   director TEXT NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS metadata_created_event(
+  id UUID PRIMARY KEY NOT NULL,
+  record_type TEXT NOT NULL,
+  event_type TEXT NOT NULL
+);
---
+++ metadata/internal/repository/postgres/metadata_event_repository.go
@@ -0,0 +1,68 @@
+package postgres_repo
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/karaMuha/go-movie/metadata/internal/core/ports/driven"
+	metadataModel "github.com/karaMuha/go-movie/metadata/pkg"
+)
+
+type MetadataEventRepository struct {
+	db *sql.DB
+}
+
+func NewMetadataEventRepository(db *sql.DB) MetadataEventRepository {
+	return MetadataEventRepository{
+		db: db,
+	}
+}
+
+var _ driven.IMetadataEventRepository = (*MetadataEventRepository)(nil)
+
+func (r *MetadataEventRepository) Save(ctx context.Context, event *metadataModel.MetadataEvent) error {
+	query := `
+		INSERT INTO metadata_creted_event(id, record_ype, event_type)
+		VALUES($1, $2, $3);
+	`
+	_, err := r.db.ExecContext(ctx, query, event.ID, event.RecordType, event.EventType)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *MetadataEventRepository) Load(ctx context.Context) (*[]metadataModel.MetadataEvent, error) {
+	query := `
+		SELECT *
+		FROM metadata_created_event
+	`
+	rows, err := r.db.QueryContext(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+
+	var events []metadataModel.MetadataEvent
+	for rows.Next() {
+		var event metadataModel.MetadataEvent
+		if err := rows.Scan(
+			&event.ID,
+			&event.RecordType,
+			&event.EventType,
+		); err != nil {
+			return nil, err
+		}
+		events = append(events, event)
+	}
+	return &events, nil
+}
+
+func (r *MetadataEventRepository) Delete(ctx context.Context, ID string) error {
+	query := `
+		DELETE FROM metadata_created_event
+		WHERE id = $1
+	`
+	_, err := r.db.ExecContext(ctx, query, ID)
+	return err
+}
